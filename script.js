0e745649058c3816e65b94c78bb36948260ccf58
bandardeterjen <bandardeterjen@gmail.com>
2025-02-12 11:25:12 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// FAQ Dropdown
+document.querySelectorAll('.faq-question').forEach(item => {
+    item.addEventListener('click', () => {
+        const answer = item.nextElementSibling;
+        if (answer.style.display === 'block') {
+            answer.style.display = 'none';
+        } else {
+            answer.style.display = 'block';
+        }
+    });
+});
